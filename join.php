b601730f4d1a7b55d5b980b5e1975ef208777dda
realServerRack <crafting4ever1@gmail.com>
2018-12-16 15:43:52 -0500

    Master Upload
    Background image credit to DiscordApp
    Logo credit to the CosmicSky Twitter

---
+++ join.php
@@ -0,0 +1,62 @@
+<head>
+<title>Cosmic Sky Community Discord - Unofficial</title>
+<style>
+img {
+  border-radius: 50%;
+}
+.button {
+	background-color:#555555;
+	font-size: 24px;
+	padding: 12px;
+	border-radius: 12px;
+	color: orange;
+	border: 2px solid #555555;
+	box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+}
+.button:hover {
+  background-color: orange; /* Green */
+  color: black;
+  border: orange;
+}
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #555554;
+  color: white;
+  text-align: center;
+}
+a:link {
+  text-decoration: none;
+  color: white;
+}
+
+a:visited {
+  text-decoration: none;
+  color: white;
+}
+
+a:hover {
+  text-decoration: underline;
+  color: white;
+}
+
+a:active {
+  text-decoration: none;
+  color: white;
+}
+</style>
+</head>
+<body background="background.jpg">
+<center>
+	<br><br><br><br><br><br>
+	<img src="logo.jpg" alt="logo_circle">
+	<br><br><br><br><br><br><br><br><br><br>
+	<a href="https://discord.gg/qGcXDuE"><button class="button button">Connect to the CosmicSky Discord</button></a>
+</center>
+</body>
+<div class="footer">
+<p>Designed with ❤ by H. Lavoie<p>
+<p>Like this design? Check it out on my <a href="https://github.com/realServerRack/discord-invite">GitHub</a>!
+</div>
